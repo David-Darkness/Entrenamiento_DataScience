34c107de8c06eae98876d17d3f821d141c7d68e6
Juan David <polancoortizjuandavid2@gmail.com>
2026-09-22 21:09:44 -0500

    feat(dataset_check): add dataset check script

--- dataset_check.py
+++ dataset_check.py
@@ -17,3 +17,17 @@ for class_id, class_name in enumerate(data["names"]):
     print(f"{class_id}: {class_name}")
 
 
+# Carpetas de imágenes
+train_images = DATASET_DIR / "train" / "images"
+valid_images = DATASET_DIR / "valid" / "images"
+test_images = DATASET_DIR / "test" / "images"
+
+# Contar imágenes
+train_count = len(list(train_images.iterdir()))
+valid_count = len(list(valid_images.iterdir()))
+test_count = len(list(test_images.iterdir()))
+
+print("\nCantidad de imágenes:")
+print(f"Train: {train_count}")
+print(f"Valid: {valid_count}")
+print(f"Test: {test_count}")
